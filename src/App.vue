08962187ab270c780116b59b48499b8b079ca36e
Raz Elbaz <razelbaz123@gmail.com>
2024-12-23 23:15:18 +0200

    Project intialization- vite, vue, tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TestItem from './components/TestItem.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <TestItem input="Welcome to LBCTF!" />
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
